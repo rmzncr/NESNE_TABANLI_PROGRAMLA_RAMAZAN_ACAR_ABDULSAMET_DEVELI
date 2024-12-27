55ade5be73e4851583f4177afb8e0922deee2a3e
ramazan acar <153485416+rmzncr@users.noreply.github.com>
2024-12-27 22:28:17 +0300

    Delete src/src directory

--- src/src/position.cpp
+++
@@ -1,5 +0,0 @@
-#include"position.h"
-Position::Position(int row,int column){
-    this->row=row;//nesne içindeki değşkene erişmek için  kullanılır
-    this->column=column;//parametrenin değerini nesne içindeki değşkene atar
-}
